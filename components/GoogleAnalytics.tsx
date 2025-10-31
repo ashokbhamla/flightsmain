285b347b3f9e8721df9addf93d4bd485bac55154
ashokbhamla <ashokbhamla@gmail.com>
2025-10-31 18:21:17 +0530

    Add Google Ads conversion tag (AW-16765334947) with phone conversion to GoogleAnalytics component using next/script

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -3,6 +3,8 @@
 import Script from 'next/script';
 
 const GA_TRACKING_ID = 'G-38CR0JRFNT';
+const ADS_ID = 'AW-16765334947';
+const ADS_PHONE_LABEL = 'QHicCJr_1rYbEKPrqro-';
 
 export default function GoogleAnalytics() {
   return (
@@ -10,6 +12,11 @@ export default function GoogleAnalytics() {
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      {/* Google Ads tag */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${ADS_ID}`}
       />
       <Script
         id="google-analytics"
@@ -22,6 +29,12 @@ export default function GoogleAnalytics() {
             gtag('config', '${GA_TRACKING_ID}', {
               page_title: document.title,
               page_location: window.location.href,
+            });
+            // Google Ads base config
+            gtag('config', '${ADS_ID}');
+            // Phone call conversion
+            gtag('config', '${ADS_ID}/${ADS_PHONE_LABEL}', {
+              'phone_conversion_number': '(888) 319-6206'
             });
           `,
         }}
